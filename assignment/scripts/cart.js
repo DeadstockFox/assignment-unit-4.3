1cd3a27fe1ee267c28269444cd276fd3f4f680ad
Michael Kenyon <michaelkenyon2022@gmail.com>
2024-02-01 21:01:10 -0600

    Close

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,8 +1,61 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+const maxItems = 5;
+
+function addItem(item){
+    if (basket.length < maxItems){
+       basket.push(item);
+       return true;
+     }
+     else if ( basket.length >= maxItems){
+      return false;
+     }
+   }
+
+addItem('taco');
+addItem('berry');
+addItem('pizza');
+console.log(basket);
+
+function listItems() {
+for (item of basket){
+    console.log(item);
+}
+}
+listItems()
+
+function empty(){
+while(basket.length > 0){
+    basket.pop();
+    console.log(basket);
+}
+}
+empty();
+
+addItem('taco');
+addItem('berry');
+addItem('pizza');
+addItem('tacos');
+addItem('berries');
+addItem('pizzas');
 
 
+function isFull (){
+    if (basket.length < maxItems){
+         return false;
+   }
+   else if( basket.length >= maxItems){
+    return true;
+   }
+}
+console.log(isFull());
+
+function removeItem(items){
+    basket.indexOf(items)
+}
 
 
 
